61bbdaf71ee8316c5a472d8e0da19fd89670ef04
N1kitaKorotkov <n1kitakorot08@yandex.ru>
2026-09-18 01:10:50 +0300

    small changes

---
+++ frontend/dist/assets/useLegacyCards-faRkeoIG.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CXQVuTEd.js";import{t}from"./usePersistentState-BkBcs7ME.js";import{c as n}from"./legacyUi-D-Ij2Ixf.js";function r(){let[r,i]=t(`legacy.cards`,n),a=e();return{cards:r,moveCard:(e,t)=>{i(n=>n.map(n=>n.id===e?{...n,stage:t}:n)),a.success(`Статус взаимодействия обновлён`)}}}export{r as t};
